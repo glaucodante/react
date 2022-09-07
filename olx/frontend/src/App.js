5700c88bb39308a21f56b1ab4d27e2fd170ba9b5
Glauco Dante <108493608+glaucodante@users.noreply.github.com>
2022-09-07 13:58:00 -0300

    Inserindo file olx

---
+++ olx/frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { connect } from "react-redux";
+
+
+const Page = (props) => {
+  return (
+    <div>Opa, funcionando...</div>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Page);
